ba4b13c99e47c2c246f61261fad28724bead89c8
Xanothos <xanothos@gmail.com>
2017-05-07 10:57:46 +0000

    Part 4 - added a service
    The data service was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent }  from './app.component';
 
